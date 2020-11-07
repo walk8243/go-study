466eff802ba963ef73e9bf9ca305b058302e082c
のぶ <walk8243@gmail.com>
2020-11-07 23:34:17 +0900

    関数を作ってみる

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+func f(a int, b int) (int, int) {
+	c := a + b
+	d := a * b
+	return c, d
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	a := 1
+	b := 2
+	c, d := f(a, b)
+	fmt.Println("Hello, World!", c, d)
 }
