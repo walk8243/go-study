57fef845222e4a4522ec2c852b5015f9faa2740e
walk8243 <walk8243@gmail.com>
2020-11-18 07:36:43 +0900

    for文や配列を使ってみる

--- calc.go
+++ calc.go
@@ -5,3 +5,25 @@ func f(a int, b int) (int, int) {
 	d := a * b
 	return c, d
 }
+
+func getPrimeNumber(maxNum int) ([]int) {
+	primes := [] int{}
+	for i := 2; i < maxNum; i++ {
+		if isPrimeNumber(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func isPrimeNumber(number int) (bool) {
+	max := number / 2
+	flag := true
+	for i := 2; i <= max; i++ {
+		if number % i == 0 {
+			flag = false
+			break
+		}
+	}
+	return flag
+}
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	a := 1
-	b := 2
-	c, d := f(a, b)
-	fmt.Println("Hello, World!", c, d)
+	digit := 3
+
+	number := int(math.Pow(10, float64(digit))) - 1
+	primes := getPrimeNumber(number)
+	fmt.Println(primes)
 }
